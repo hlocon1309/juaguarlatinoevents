ae45c9f30878c3d658f7adf7d9b95ad13938cd6d
Haroldo Locon <rhlocon@gmail.com>
2024-07-21 02:32:33 +0300

    Login Updated

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 import psycopg2
 
 app = Flask(__name__)
+app.secret_key = 'jaguarlatinoevents'
 
 @app.route('/') 
 def index(): 
     # Connect to the database 
-    conn = psycopg2.connect(database = "testdb",
+    conn = psycopg2.connect(database="testdb",
                         user="haroldo",
                         password="Russi@2021",
                         host="localhost",
@@ -18,15 +19,93 @@ def index():
     # Select first three elements from active events to show
     cur.execute('''select events.event_name, to_char(events.start_date, 'DD-MM-YYYY'), event_data.sale_price from events inner join event_data on events.id_event=event_data.id_event limit 3;''') 
   
-    # Fetch the data 
-    data = cur.fetchall() 
+    # Fetch the data from events
+    evnt = cur.fetchall() 
+    print(evnt)
   
     # close the cursor and connection 
     cur.close() 
-    conn.close() 
-  
-    return render_template('index.html', data=data)
+    conn.close()
 
+    if 'username' in session:
+        return render_template('index.html', data=evnt, username=session['username'])
+    else:
+        return render_template('index.html', data=evnt)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        #return '<h1>Page Found but not Implemented</h1>'
+        username = request.form['demail']
+        pwd = request.form['dpassword']
+
+        # Connect to the database 
+        conn = psycopg2.connect(database="testdb",
+                    user="haroldo",
+                    password="Russi@2021",
+                    host="localhost",
+                    port="5432")
+        # create a cursor 
+        cur = conn.cursor() 
+        
+        cur.execute('''select user_name, user_mail, user_passw from users where user_mail = %s; ''', (username,) )
+        
+        # Fetch the data from events
+        user = cur.fetchone()
+        print(user)
+
+        # close the cursor and connection 
+        cur.close() 
+        conn.close()
+
+        if user and pwd == user[2]:
+            session['username'] = user[0]
+            return redirect("/#home")
+        else:
+            return render_template('login.html', error='Invalid usermail and password')
+     
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        #return '<h1>Page Found but not Implemented</h1>'
+        uname = request.form['duser']
+        username = request.form['demail']
+        pwd = request.form['dpassword']
+
+        # Connect to the database 
+        conn = psycopg2.connect(database="testdb",
+                    user="haroldo",
+                    password="Russi@2021",
+                    host="localhost",
+                    port="5432")
+        # create a cursor 
+        cur = conn.cursor() 
+        
+        cur.execute('''insert into users (user_name, user_surname, user_mail, user_passw, user_phone, date_created, user_type) values \
+                    (%s, ' ', %s, %s, '+', '2024-07-20 08:00:00', 3);''', (uname, username, pwd) )
+        
+        # Fetch the data from events
+        # commit the changes 
+        conn.commit() 
+
+        # close the cursor and connection 
+        cur.close() 
+        conn.close()
+
+        return redirect("/#home")
+      
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/#home')
+    
 
 
 
--- static/script.js
+++ static/script.js
@@ -34,3 +34,17 @@ var swiper = new Swiper(".home-slider", {
         disableOnInteraction:false,
     }
   });
+
+
+const signInBtnLink = document.querySelector('.signInBtn-link');
+const signUpBtnLink = document.querySelector('.signUpBtn-link');
+const wrapper = document.querySelector('.wrapper');
+
+signUpBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+})
+
+signInBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+})
+
